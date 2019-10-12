6302ff77adcbb64eaa164096f1e7543fd40f2b95
Mike Liao <mike.hm.liao@gmail.com>
2019-10-13 00:38:59 +0800

    built landmark list view

---
+++ Landmark/Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  Landmark
+//
+//  Created by Mike Liao on 2019/10/13.
+//  Copyright © 2019 Mike Liao. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+                
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
--- Landmark/Views/MapView.swift
+++ Landmark/Views/MapView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    var coordinate: CLLocationCoordinate2D
+    
     func makeUIView(context: Context) -> MKMapView {
         MKMapView(frame: .zero)
     }
     
     func updateUIView(_ view: MKMapView, context: Context) {
-        let coordinate = CLLocationCoordinate2D(latitude: 25.044, longitude: 121.529)
         let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         view.setRegion(region, animated: true)
@@ -24,6 +25,6 @@ struct MapView: UIViewRepresentable {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: landmarkData[0].locationCoordinate)
     }
 }
