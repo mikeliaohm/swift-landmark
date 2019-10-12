975045f255a817caac24102fe55ee8191bc73544
Mike Liao <mike.hm.liao@gmail.com>
2019-10-12 22:53:06 +0800

    Initial Commit

---
+++ Landmark/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmark
+//
+//  Created by Mike Liao on 2019/10/12.
+//  Copyright © 2019 Mike Liao. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ Landmark/MapView.swift
@@ -0,0 +1,29 @@
+//
+//  MapView.swift
+//  Landmark
+//
+//  Created by Mike Liao on 2019/10/12.
+//  Copyright © 2019 Mike Liao. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(latitude: 25.044, longitude: 121.529)
+        let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
