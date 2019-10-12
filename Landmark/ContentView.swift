fd37f57f3d8cfc615025b249602bfb96988d8d55
Mike Liao <mike.hm.liao@gmail.com>
2019-10-12 23:46:27 +0800

    added models

--- Landmark/ContentView.swift
+++ Landmark/ContentView.swift
@@ -10,33 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        VStack {
-            MapView()
-                .edgesIgnoringSafeArea(.top)
-                .frame(height:300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                
-                HStack(alignment: .top) {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
-                }
-            }
-            .padding()
-            
-            Spacer()
-        }
-        
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
