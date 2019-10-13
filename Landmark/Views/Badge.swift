72b64b215556f6fbe672c75ace297442d453339b
Mike Liao <mike.hm.liao@gmail.com>
2019-10-13 13:34:54 +0800

    finished badge

--- Landmark/Views/Badge.swift
+++ Landmark/Views/Badge.swift
@@ -9,17 +9,32 @@
 import SwiftUI
 
 struct Badge: View {
+    static let rotationCount = 8
+    
     var badgeSymbols: some View {
-        RotatedBadgeSymbol(angle: .init(degrees: 0))
-            .opacity(0.5)
+        
+        ForEach(0..<Badge.rotationCount) { i in
+            
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(i)
+                    / Double(Badge.rotationCount)) * 360.0
+            )
+        }
+        .opacity(0.5)
     }
     
     var body: some View {
         ZStack {
             BadgeBackground()
-            self.badgeSymbols
+            
+            GeometryReader { geometry in
+                
+                self.badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
         }
-        
+        .scaledToFit()
     }
 }
 
