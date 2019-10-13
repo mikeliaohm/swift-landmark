19b1c6c479792905ef4d49c66e41b4049d7d2ec2
Mike Liao <mike.hm.liao@gmail.com>
2019-10-13 15:00:44 +0800

    finished home view

--- Landmark/Views/CategoryRow.swift
+++ Landmark/Views/CategoryRow.swift
@@ -22,7 +22,10 @@ struct CategoryRow: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .top, spacing: 0) {
                     ForEach(self.items) { landmark in
-                        CategoryItem(landmark: landmark)
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)
+                        ) {
+                            CategoryItem(landmark: landmark)
+                        }
                     }
                 }
             }
@@ -36,11 +39,13 @@ struct CategoryItem: View {
     var body: some View {
         VStack(alignment: .leading) {
             landmark.image
+                .renderingMode(.original)
                 .resizable()
                 .frame(width: 155, height: 155)
                 .cornerRadius(5)
             
             Text(landmark.name)
+                .foregroundColor(.primary)
                 .font(.caption)
         }
         .padding(.leading)
--- Landmark/Views/Landmark/LandmarkList.swift
+++ Landmark/Views/Landmark/LandmarkList.swift
@@ -12,25 +12,22 @@ struct LandmarkList: View {
     @EnvironmentObject var userData: UserData
     
     var body: some View {
-        NavigationView {
+        
+        List {
+            Toggle(isOn: $userData.showFavoriteOnly) {
+                Text("Favorites only")
+            }
             
-            
-            List {
-                Toggle(isOn: $userData.showFavoriteOnly) {
-                    Text("Favorites only")
+            ForEach(userData.landmarks) { landmark in
+                if !self.userData.showFavoriteOnly || landmark.isFavorite {
+                    
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
                 
-                ForEach(userData.landmarks) { landmark in
-                    if !self.userData.showFavoriteOnly || landmark.isFavorite {
-                        
-                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                            LandmarkRow(landmark: landmark)
-                        }
-                    }
-                    
-                }
-                .navigationBarTitle(Text("Landmarks"))
             }
+            .navigationBarTitle(Text("Landmarks"))
         }
     }
 }
@@ -38,10 +35,12 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .environmentObject(UserData())
-                // make sure environment object is added to preview as well
+            NavigationView {
+                LandmarkList()
+                    .previewDevice(PreviewDevice(rawValue: deviceName))
+                    .environmentObject(UserData())
+            }
+            // make sure environment object is added to preview as well
         }
     }
 }
