17078c1f6b7c507d9c35cada9c536e8897212f4e
Mike Liao <mike.hm.liao@gmail.com>
2019-10-13 12:45:52 +0800

    combining badge symbols with background

---
+++ Landmark/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmark
+//
+//  Created by Mike Liao on 2019/10/13.
+//  Copyright © 2019 Mike Liao. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
